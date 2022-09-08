65aebf8949024361944f82e4194643e659a2b20e
Marthy28 <titou.mary@gmail.com>
2022-09-08 08:27:12 +0200

    Ajout du drawer

--- app/src/main/java/com/example/dndcharactergenerator/MainActivity.kt
+++ app/src/main/java/com/example/dndcharactergenerator/MainActivity.kt
@@ -3,25 +3,50 @@ package com.example.dndcharactergenerator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.dndcharactergenerator.theme.MyApplicationTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyApplicationTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val scaffoldState = rememberScaffoldState()
+                val scope = rememberCoroutineScope()
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {
+                        TopAppBar(title = { Text(text = "D&D generator") },
+                            navigationIcon = {
+                                IconButton(onClick = { //showMenu = !showMenu
+                                    scope.launch {
+                                        scaffoldState.drawerState.open()
+                                    }
+                                }) {
+                                    Icon(Icons.Default.Menu, "Menu")
+                                }
+                            })
+                    },
+                    drawerContent = {
+                        Column() {
+                            Text("prout")
+                            Text("Yolo")
+                            //Créer un comopsant pour le menu Item
+                        }
+                    })
+                {
+                    Column(modifier = Modifier.padding(it)) {
+                        Greeting(name = "Thomas")
+                    }
                 }
             }
         }
---
+++ app/src/main/java/com/example/dndcharactergenerator/ui/component/CustomDrawer.kt
@@ -0,0 +1,8 @@
+package com.example.dndcharactergenerator.ui.component
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun DrawerBody() {
+
+}
